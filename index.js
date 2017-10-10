5029bbfec18b3e6b356edb2430411bb7e46c1b95
Nick Mattise <n.mattise@gmail.com>
2017-10-10 17:32:37 -0400

    stupid ;

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = {
     Polygon : require('./src/polygon.js'),
     Vector: require('./src/vector.js').Vector,
     Vectors: require('./src/vector.js').Vectors,
-    Line: require('./src/line.js');
+    Line: require('./src/line.js')
 }
