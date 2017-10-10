b21a857af5e16a162e166fe1826c032fc7b8af19
Nick Mattise <n.mattise@gmail.com>
2017-10-10 17:29:41 -0400

    expose all parts

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 module.exports = {
-    Polygon : require('./src/polygon.js')
+    Polygon : require('./src/polygon.js'),
+    Vector: require('./src/vector.js').Vector,
+    Vectors: require('./src/vector.js').Vectors,
+    Line: require('./src/line.js');
 }
